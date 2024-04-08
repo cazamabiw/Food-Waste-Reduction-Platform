df9e5013b1d4376c281354b24d6c555916f12390
Sreelakshmi-Odatt <sreelakshmiodatt@gmail.com>
2024-04-08 18:21:15 -0400

    update user model

---
+++ Food Waste Reduction Platform/src/java/com/fwrp/models/Role.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fwrp.models;
+
+/**
+ * @author Sreelakshmi Odatt Venu
+ */
+public class Role {
+    private int roleId;
+    private String roleName;
+
+    public Role(int roleId,String roleName){
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+    // Getters and Setters
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
