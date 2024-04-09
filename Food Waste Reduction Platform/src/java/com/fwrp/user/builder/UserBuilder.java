e3097b98fc00df1fab04adcfeccca5476de8d751
Sreelakshmi-Odatt <sreelakshmiodatt@gmail.com>
2024-04-08 21:10:04 -0400

    Added builder and factory classes
    Added  and  updated builder and factory classes .

--- Food Waste Reduction Platform/src/java/com/fwrp/user/builder/UserBuilder.java
+++ Food Waste Reduction Platform/src/java/com/fwrp/user/builder/UserBuilder.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.fwrp.datatier.dao;
+package com.fwrp.user.builder;
+
+import com.fwrp.models.User;
 
 /**
  *
- * @author sreel
+ * @author Sreelakshmi Odatt Venu
  */
-public class test {
-    
+public interface UserBuilder {
+    User build();
 }
