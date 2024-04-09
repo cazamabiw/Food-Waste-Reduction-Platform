001ad1f84a9b9599da4429b5c11fec2e37cf2d4e
weirdguy01 <107738377+weirdguy01@users.noreply.github.com>
2024-04-08 21:56:16 -0400

    Create Action Log
    Create Action Log

---
+++ Food Waste Reduction Platform/src/java/com/fwrp/models/ActionLog.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fwrp.models;
+
+/**
+ *
+ * @author Jessica Gunawan
+ */
+import java.util.Date;
+
+public class ActionLog {
+
+    private int id;
+    private int userId;
+    private String userAction;
+    private Date actionDate;
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserAction() {
+        return userAction;
+    }
+
+    public void setUserAction(String userAction) {
+        this.userAction = userAction;
+    }
+
+    public Date getActionDate() {
+        return actionDate;
+    }
+
+    public void setActionDate(Date actionDate) {
+        this.actionDate = actionDate;
+    }
+}
